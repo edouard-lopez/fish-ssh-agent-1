6c55347e43a51d28154c17e35f61372c6028db24
Marc Garcia Sastre <markcial@gmail.com>
2016-09-28 15:39:26 +0200

    Testing identities added in ssh

--- test/add_keys.fish
+++ test/add_keys.fish
@@ -19,15 +19,12 @@ test "$TEST - Fail if `keys` list is empty"
     ) = 1
 end
 
-
 test "$TEST - Add given keys"
     (
-		ssh-agent /usr/bin/fish
-    	set old_keys_list (ssh-add -l | wc -l)
-		ssh-keygen -f $path/.test -N '' >/dev/null
-		__add_keys $path/.test.pub
-        set new_keys_list (ssh-add -l | wc -l)
-    	
-		math $new_key_list - $old_keys_list
-    ) = 1
+		  eval (ssh-agent -c) > /dev/null
+      ssh-add -l
+    	ssh-keygen -f $path/.test -N "" > /dev/null
+      __add_keys $path/.test.pub > /dev/null
+      ssh-add -L | awk -F' ' '{print $2}'
+    ) = "The agent has no identities."  (cat $path/.test.pub| awk -F ' ' '{print $2}')
 end
